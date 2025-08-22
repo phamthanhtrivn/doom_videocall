dc2ed15b96c321f5cf0813b56f959383ea79ebfa
Tri Pham <phamthanhtri0712@gmail.com>
2025-08-22 21:42:54 +0700

    update time and date in home page

--- src/app/(root)/(home)/page.tsx
+++ src/app/(root)/(home)/page.tsx
@@ -4,17 +4,8 @@ import { Bell } from "lucide-react";
 const Home = () => {
   const now = new Date();
 
-  const time = now.toLocaleTimeString("en-US", {
-    hour: "2-digit",
-    minute: "2-digit",
-    hour12: true,
-  });
-  const date = now.toLocaleDateString("en-US", {
-    weekday: "long",
-    month: "long",
-    day: "numeric",
-    year: "numeric",
-  });
+  const time = now.toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit' });
+  const date = (new Intl.DateTimeFormat('en-US', { dateStyle: 'full' })).format(now);
 
   return (
     <section className="flex flex-col gap-5 text-white size-full">
